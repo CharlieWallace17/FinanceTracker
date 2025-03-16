20c9726f5f27cf4e268142968ffbdeb2bb62d667
Charlie <charlescwallace2017@gmail.com>
2025-03-16 12:42:00 -0400

    improvements;

--- FinanceTrackerWebsite/src/App.tsx
+++ FinanceTrackerWebsite/src/App.tsx
@@ -6,7 +6,7 @@ import { useForm } from 'react-hook-form'
 function App() {
   const [ticker, setTicker] = useState(undefined);
 
-  const { data } = useQuery({ queryKey: ['getStockPrice'], queryFn: () => fetch(`${import.meta.env.VITE_SERVER_URL}/tickerInfo/${ticker}`).then(response => response.json()), enabled: !!ticker })
+  const { data, isFetching, refetch, isPending, isSuccess, isRefetching } = useQuery({ queryKey: ['getStockPrice', ticker], queryFn: () => fetch(`${import.meta.env.VITE_SERVER_URL}/tickerInfo/${ticker}`).then(response => response.json()), enabled: !!ticker })
 
   const { register, handleSubmit } = useForm({ values: { tickerValue: ticker } });
 
@@ -14,13 +14,19 @@ function App() {
   console.log(data);
   return (
     <div className="!w-full !h-screen">
-      <form className="flex flex-col" onSubmit={handleSubmit(data => setTicker(data.tickerValue))}>
-        {data && <div className="flex flex-col bg-green-500">
+      <form className="flex flex-col" onSubmit={handleSubmit(data => {
+        setTicker(data.tickerValue)
+        if (ticker)
+          refetch()
+      })}>
+        {data && !isFetching && !isPending && !isRefetching && isSuccess ? <div className="flex flex-col bg-green-500">
           <div>Current Price: {`$${data.currentPrice}`}</div>
           <div>Price Change: {`$${data.change}`}</div>
           <div>Percentage Change: {`${data.percentChange}%`}</div>
           <div>High: {`$${data.highDailyPrice}`}</div>
-        </div>}
+        </div>
+          :
+          <div>Loading...</div>}
         <input maxLength={4} className="h-7 w-40 border-2 rounded border-black" {...register("tickerValue")} placeholder="Enter stock ticker..." />
         <button type="submit" className="rounded border-solid bg-blue-100 border-2 w-20 hover:bg-blue-200 active:bg-blue-300">Click me</button>
       </form>
